f7b07783696a3f1505433b9104bab2961f1a6b74
sumayya-github <66043790+sumayya-github@users.noreply.github.com>
2020-06-28 00:30:51 +0530

    mail first commit

---
+++ mail.py
@@ -0,0 +1,12 @@
+import smtplib
+s = smtplib.SMTP('smtp.gmail.com', 587)
+s.starttls()
+
+s.login("sumayyakhatoon58@gmail.com", "summaiya@26")
+
+
+message = "Accuracy is less than 90%.Please try again"
+
+s.sendmail("sumayyakhatoon58@gmail.com", "s8355827840@gmail.com", message)
+    
+s.quit()
